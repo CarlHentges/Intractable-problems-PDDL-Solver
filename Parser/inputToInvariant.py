59c0d34c4eab4165b4c59e7b4c6069a0589c34ba
Carl Hentges <carl.hentges@kcl.ac.uk>
2019-11-27 11:42:09 -0800

    Added documentation

--- Parser/inputToInvariant.py
+++ Parser/inputToInvariant.py
@@ -1,27 +1,36 @@
-# CS 170 Group Project Problem Generator
+# CS 170 Group Project, INPUT PARSING
 # This program parses problem files for the CS170 group project and
 # converts them into a form that preserves the lowercase valuse
+# this was due to the fact that PDDL does not preserve upper and
+# lowercase, and thus it is nesisay to add additional information
+# to the lowercase characters so that the variables can be recovered
+#
 # Carl Hentges 24.11.19
 #
-# This takes a PDDL planner solution and creates an output that
-# conforms to the output specification
 
 import random
 import string
 import sys
-# gets the input from the user indicating what solution file to use\
-#PROBLEM_FILE = str(input("Enter SOLUTION_FILE file name: "))
+# gets an input as a comand line argument indicating what problem file to use
 PROBLEM_FILE = str(sys.argv[1])
+# gets an input as a comand line argument indicating the output file
 OUTPUT_FILE = str(sys.argv[2])
 # f is the input file (opened in read mode)
 f = open(PROBLEM_FILE, "r")
-# O is the ouput file (opened in write mode)
+# o is the ouput file (opened in write mode)
 o = open(OUTPUT_FILE, "w")
+
 # output will ultamately be written to the ouput file
 output = ""
+
 lineCount = 0
+# Only the Locations, TA Locations and Start location can contain Lowercase characters
+# this corresponds to lines 2 through 4
 TOPLine = 2
 BOTLine = 4
+
+# reads the file line by line and prepends each lowercase character with an underscore _
+# capital letters are left as they are
 line = f.readline()
 while line:
 	if(lineCount >= TOPLine and lineCount <= BOTLine):
@@ -34,6 +43,7 @@ while line:
 		output += line
 	lineCount +=1
 	line = f.readline()
+# Write ouput to the  output file and close the files in use
 print(output)
 o.write(output)
 o.close()
